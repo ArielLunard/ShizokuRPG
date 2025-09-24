99573d63e7c3a651938d25dfb1bca1361aa1d0a3
ARIEL LUNARDELLI <136381025+ArielLunard@users.noreply.github.com>
2025-09-23 22:26:42 -0300

    Update config.php
    Segurança eliminado update indesejado.

--- ficha/php/config.php
+++ ficha/php/config.php
@@ -1,12 +1,4 @@
 <?php
-$host = "127.0.0.1";
-$db   = "u834715996_shizoku";
-$user = "u834715996_devariel";
-$pass = "DevAri25!";
 
-$conn = new mysqli($host, $user, $pass, $db);
-
-if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
-}
 ?> 
+
